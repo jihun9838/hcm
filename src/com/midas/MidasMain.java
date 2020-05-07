390d210db0405486e1ab60abf0f92e815bd81339
jihun9838 <44036052+jihun9838@users.noreply.github.com>
2020-05-08 01:32:49 +0900

    fourth commit

--- src/com/midas/MidasMain.java
+++ src/com/midas/MidasMain.java
@@ -16,7 +16,7 @@ public class MidasMain extends Application{
 		CommonService comServ = new CommonServiceImpl();
 		
 		BorderPane borderPane = (BorderPane)comServ.AddScene("/com/midas/Homepage.fxml");
-		
+		System.out.println("Homepage");
 		// if(dbServ.getEmployee(id).getCategory() == 1)
 		//Parent empMenuScene = comServ.AddScene("/com/midas/Employee.fxml");
 		// else
@@ -25,6 +25,7 @@ public class MidasMain extends Application{
 		//Parent s = comServ.AddScene("/com/midas/salary/SalaryMgmt.fxml");
 
 		Parent ss = comServ.AddScene("/com/midas/mainpage/login.fxml");
+		System.out.println("logingpage");
 		
 		
 
